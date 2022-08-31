e9904fac6ded21bad9a8f19934ee308b6a2887f4
VikramAdytia <Rundarreg@yandex.ru>
2022-08-31 08:32:52 +0300

    task 2 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 Console.Write("num1 = ");
 int x =  int.Parse(Console.ReadLine());
 Console.Write("num2 = ");
@@ -12,19 +12,49 @@ else
 {
     Console.WriteLine($"max = {y}");
 }
+*/
+
+Console.Write("num1 = ");
+int x =  int.Parse(Console.ReadLine());
+Console.Write("num2 = ");
+int y =  int.Parse(Console.ReadLine());
+Console.Write("num3 = ");
+int z =  int.Parse(Console.ReadLine());
 
 /*
-Console.Write("day = ");
-int x = int.Parse(Console.ReadLine());
-if (x == 3)
+public static void AsIf<T>(this object value, Action<T> action) where T : class
 {
-        Console.WriteLine("Wednesday");
+    T kostyl = value as T;
+    if (x > y) {
+        //int xy = x;
+        action(kostyl);
+    }
 }
-if ( x == 5)
-{
-        Console.WriteLine("Friday");
+//error CS0136: Локальная переменная или параметр
+// с именем "xy" нельзя объявить в данной области
+//Why Isn't It Possible? Why Not?
+//не даёт использовать ни присвоеный внутри оператор
+//ни присвоить обьявленный раньше
+//error CS0103: Имя "xy" не существует
+// в текущем контексте.
+// писать екстеншн это похоже на костыль и пока для меня сложно.
+else                      {
+    int xy = y;
 }
 */
+int xy = x > y ? x : y ;
+
+if (xy > z) 
+{
+    Console.WriteLine(xy);
+}
+
+else                      
+{
+    Console.WriteLine(z);
+}
+
+
 /*
 Console.Write("n = ");
 int n = int.Parse(Console.ReadLine());
