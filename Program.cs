15f13ea8bca41daf444b67f692f9f523dcc09652
VikramAdytia <Rundarreg@yandex.ru>
2022-08-31 07:31:42 +0300

    task 1 done

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
-﻿/*
+﻿
 Console.Write("num1 = ");
 int x =  int.Parse(Console.ReadLine());
 Console.Write("num2 = ");
 int y =  int.Parse(Console.ReadLine());
 
-x = x * x;
-
-if (x == y) 
+if (x > y) 
 {
-    Console.WriteLine("yes");
+    Console.WriteLine($"max = {x}");
 }
 else                      
 {
-    Console.WriteLine("no");
+    Console.WriteLine($"max = {y}");
 }
-*/
+
 /*
 Console.Write("day = ");
 int x = int.Parse(Console.ReadLine());
@@ -27,11 +25,11 @@ if ( x == 5)
         Console.WriteLine("Friday");
 }
 */
-
+/*
 Console.Write("n = ");
 int n = int.Parse(Console.ReadLine());
 var list = numbers.ToList();
 int nRange = list.GetRange(0, n);
 Console.WriteLine(nRange);
-
+*/
 
