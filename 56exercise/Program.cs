4f7d11bf90aa52f3cd91f632f6e7e0e858056958
VikramAdytia <Rundarreg@yandex.ru>
2022-09-27 13:44:56 +0300

    midified gitignore

---
+++ 56exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿void GetMinimalSumOfRowFromArray()
+{
+    System.Console.WriteLine("Input rows number : ");
+    int n = Convert.ToInt32 (Console.ReadLine());
+    System.Console.WriteLine("Input columns number : ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    double[,] randomized2DArray = new double[n, m];
+
+    Random random = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+         randomized2DArray[i, j] = random.Next(100);
+            Console.Write(" " + randomized2DArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+    double min = 0;
+    int minIndex = 0;
+    for (int i = 0; i < n ; i++ )
+    {
+        double sum = 0;
+        for (int j = 0; j < m; j++)
+
+            {
+            sum = sum + randomized2DArray[i, j];
+            }
+        if (i == 0)
+        {
+            min = sum;
+        }
+        if (min > sum)
+        {
+            minIndex = i;
+            min = sum;
+        }
+    }
+    System.Console.WriteLine($"min sum = {min} in row {minIndex}");
+
+}
+
+GetMinimalSumOfRowFromArray();
---
+++ 58exercise/Program.cs
@@ -0,0 +1,71 @@
+﻿int[,] GetProductOfTwoMatrix()
+{
+    System.Console.WriteLine("Input rows number : ");
+    int n = Convert.ToInt32 (Console.ReadLine());
+    System.Console.WriteLine("Input columns number and rows : ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Input columns number : " );
+    int p =  Convert.ToInt32(Console.ReadLine());
+
+    int[,] twoDimentionalRandomizedintArray = new int[n, m];
+
+    Random random = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            twoDimentionalRandomizedintArray[i, j] = random.Next(10);
+            Console.Write(" " + twoDimentionalRandomizedintArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+        Console.WriteLine();
+
+    int[,] twoDimentionalRandomizedintArray2 = new int[m, p];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            twoDimentionalRandomizedintArray2[i, j] = random.Next(10);
+            Console.Write(" " + twoDimentionalRandomizedintArray2[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+        Console.WriteLine();
+
+    int[,] resultMatrix = new int[m,p];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+        int sum = 0;
+            for (int k = 0; k < m; k++)
+            {
+                sum += twoDimentionalRandomizedintArray[i,k] * twoDimentionalRandomizedintArray2[k,j];
+            }
+        resultMatrix[i,j] = sum;
+        }
+    }
+    
+    Console.WriteLine();
+
+    for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                Console.Write(" " + resultMatrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+
+    return resultMatrix;
+
+}
+
+
+GetProductOfTwoMatrix();
